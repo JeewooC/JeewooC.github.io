439d7cb268b0d4e45af05d1a966e6a9108bcd3ac
_Kevin_Zhang_ <49049813+Dragolla@users.noreply.github.com>
2021-03-28 03:57:57 -0700

    start 3rd vid

---
+++ src/containers/feed/index.js
@@ -0,0 +1,15 @@
+import Post from "../post";
+
+
+export default function Feed() {
+    
+    return (
+        <div className="feed">
+            <Post profileUrl ="https://firebasestorage.googleapis.com/v0/b/viridi-c4949.appspot.com/o/images%2FrduIGHcLIN.jpg?alt=media&token=0a27022d-e129-4203-ab91-a2f3f16974dc" 
+            username ="testuser"
+            />
+            
+        </div>
+    
+    )
+}
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import "./style.css"
-import { CreatePost, Navbar } from '../../containers'
+import { CreatePost, Navbar, Post } from '../../containers'
+import Feed from '../../containers/feed'
 
 export default function Home() {
     return (
         <div className="home">
            <Navbar />
            <CreatePost />
+           <Feed/>
          </div>
     )
 }
