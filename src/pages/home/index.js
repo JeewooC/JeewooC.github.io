2a94d37ba351ea2eb2a4ac5f66832cdecab896c4
Jeewoo Chung <awesomejwc@icloud.com>
2021-03-28 01:19:35 -0700

    Updated comment-input component

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import "./style.css"
 import { CreatePost, Navbar } from '../../containers'
+import { CommentInput } from '../../components'
 
 export default function Home() {
     return (
         <div className="home">
            <Navbar />
            <CreatePost />
+           <CommentInput />
          </div>
     )
 }
