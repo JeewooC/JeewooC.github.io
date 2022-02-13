23a2670df817a805d0ce3b7f962cf4252a7705d2
Jeewoo Chung <awesomejwc@icloud.com>
2022-02-13 01:33:59 -0800

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,20 @@
 import firebase from 'firebase'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBrE6LUNKCtFgvaLjNKEZTfJlpKofHMlqk",
-    authDomain: "viridi-c4949.firebaseapp.com",
-    projectId: "viridi-c4949",
-    storageBucket: "viridi-c4949.appspot.com",
-    messagingSenderId: "813233171406",
-    appId: "1:813233171406:web:2b02551aff42907ecd199f",
-    measurementId: "G-NTDH7V69M2"
-};
+  apiKey: 'AIzaSyBrE6LUNKCtFgvaLjNKEZTfJlpKofHMlqk',
+  authDomain: 'viridi-c4949.firebaseapp.com',
+  projectId: 'viridi-c4949',
+  storageBucket: 'viridi-c4949.appspot.com',
+  messagingSenderId: '813233171406',
+  appId: '1:813233171406:web:2b02551aff42907ecd199f',
+  measurementId: 'G-NTDH7V69M2',
+}
 
-const firebaseApp = firebase.initializeApp(firebaseConfig);
-const db = firebaseApp.firestore();
-const auth = firebase.auth();
-const storage = firebase.storage();
-const provider = new firebase.auth.GoogleAuthProvider();
+const firebaseApp = firebase.initializeApp(firebaseConfig)
+const db = firebaseApp.firestore()
+const auth = firebase.auth()
+const storage = firebase.storage()
+const provider = new firebase.auth.GoogleAuthProvider()
 
-export { db, auth, storage, provider };
+export { db, auth, storage, provider }
+
